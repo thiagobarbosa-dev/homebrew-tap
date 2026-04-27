afea724a337308c1ca8bbe310f731fc36a6ffaf3
Thiago Barbosa <thiagobarbosa.dev@icloud.com>
2026-04-27 19:16:02 -0300

    feat: update whatscli to v1.1.2

--- Formula/whatscli.rb
+++ Formula/whatscli.rb
@@ -1,8 +1,8 @@
 class Whatscli < Formula
   desc "WhatsApp Web CLI powered by Baileys"
   homepage "https://github.com/thiagobarbosa-dev/whatscli"
-  url "https://github.com/thiagobarbosa-dev/whatscli/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "4b9e21322f42e830e22e5c18ccd5ffcd00d6a585febc181cce74314ce0eca634"
+  url "https://github.com/thiagobarbosa-dev/whatscli/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "f8e836591ec296120f3ed68cad3f6e02415a1b2f73326720afe91a7940db11e8"
   license "MIT"
 
   depends_on "node"
